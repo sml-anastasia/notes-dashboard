c797a9871fa728937ddd13ea5dc1d463a2d01df6
anastasia <smolina.anastasia.14@gmail.com>
2021-02-25 22:50:32 +0300

    fix: fix delete() function

--- assets/script/notes.js
+++ assets/script/notes.js
@@ -1,7 +1,6 @@
 let notes = []; //массив для текста заметок
 let titles = []; //массив для заголовков
 let generatedNotes = []; //массив для созданного дива
-let noteString = ''; //переменная для генерируемой заметки
 
 document.addEventListener("DOMContentLoaded", function (event) {
     let addedNote = localStorage.getItem('note'); //переменная для ключа в хранилище
@@ -20,7 +19,7 @@ function addNote() { //функция добавления заметки
     let note = document.getElementById("add-box__note-input").value; //переменная для введенного текста заметки
     titles.push(title); //добавление в массив
     notes.push(note); //добавление в массив
-    
+    let noteString = ''; //переменная для генерируемой заметки
     for (i = 0; i < notes.length; i++) { //перебираем каждый элемент массива добавленных текстов, цикл с счетчиком, потому что нужно склеить массив заголовков и массив текстов
         noteString = `<div id="note${i}" class="note"><h3 id="note-title">${title}</h3><p id="note-text">${note}</p><button id="note-btn" type="button" onclick="deleteNote(${i});"><img src="assets/images/note-icon-delete.svg" alt="delete"></button></div>`; //создаем заметку в виде дива с заголовком и кнопкой удалить (еще в процессе)
     }
@@ -32,9 +31,22 @@ function addNote() { //функция добавления заметки
 
 function clearAll() { //функция очищения всех заметок из хранилища и из документа
     localStorage.removeItem('note'); //очищаем ключ
-    document.querySelectorAll(".note").forEach(note => { note.style.display = "none" }); //выбираем все созданные дивы, перебираем и присваиваем каждому стиль display: none;
+    document.querySelectorAll('.note').forEach(note => { note.style.display = "none" }); //выбираем все созданные дивы, перебираем и присваиваем каждому стиль display: none;
     //нагуглила просто замечательный способ
 }
+
+function deleteNote(id) {
+    generatedNotes = JSON.parse(localStorage.getItem('note'));
+    generatedNotes.splice(id, 1);
+    if (generatedNotes.length == 1) {
+        generatedNotes = [];
+    }
+    document.querySelector("#note" + id).style.display = "none";
+    localStorage.setItem('note', JSON.stringify(generatedNotes));
+}
+
+
+/*
 
 function deleteNote(id) {
     generatedNotes = JSON.parse(localStorage.getItem('note'));
@@ -44,3 +56,5 @@ function deleteNote(id) {
         localStorage.setItem('note', JSON.stringify(generatedNotes));
     }
 }
+
+*/
