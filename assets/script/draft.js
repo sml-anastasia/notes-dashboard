4ca63a12d36f7dda9ebf05cecabf5e1487b57a58
anastasia <smolina.anastasia.14@gmail.com>
2021-02-25 16:51:48 +0300

    feat: add delete one note function (need to fix)

--- assets/script/draft.js
+++ assets/script/draft.js
@@ -5,3 +5,83 @@ let elems = document.getElementsByClass("note");
 for (let i = 0; i < elems.length; i++)
 elems[i].style.display = "none";
 */
+
+
+
+
+/*for (let i = 0; i < generatedNotes.length; i++) {
+        if (generatedNotes[i].id == id) {
+            generatedNotes.splice(id, 1);
+        }
+    }
+    
+    
+    localStorage.setItem('note', JSON.stringify(generatedNotes));
+//let indexOfNoteToDelete = generatedNotes.findIndex((noteInArray) => noteInArray.id == id);
+    //console.log(indexOfNoteToDelete);
+    //let updatedNotes = generatedNotes.splice(indexOfNoteToDelete, 1);
+    //console.log(updatedNotes);
+    //
+    //document.querySelector(".note").this.style.display = "none";
+function removeItemFromTodos(itemId, todos) {
+  // find the index of the todo with the id you are looking for
+const indexOfTodoToDelete = todos.findIndex((todoInArray) => todoInArray.id === itemId));
+  // remove that todo:
+  todos.splice(indexOfTodoToDelete, 1) // delete the todo
+} 
+
+    localStorage.setItem('note', JSON.stringify(generatedNotes));
+    
+    
+    
+    
+        //let indexNote = generatedNotes.indexOf(noteToDelete);
+    //console.log(indexNote);
+    /*if (indexNote > -1) {
+        generatedNotes.splice(indexNote, 1);
+        }
+    console.log(generatedNotes);
+    
+    console.log(indexNote);
+    //generatedNotes = generatedNotes.filter(item => item != noteToDelete);
+    
+    //let deletedNote = generatedNotes.find(noteToDelete => noteToDelete.id == this.index);*/
+    /*
+let value = 3
+
+let arr = [1, 2, 3, 4, 5, 3]
+
+arr = arr.filter(item => item !== value)
+
+console.log(arr)
+// [ 1, 2, 4, 5 ]
+
+
+
+    var array = [1, 3, 5];
+console.log(array);
+// array = [1, 3, 5]
+var index = array.indexOf(3);
+if (index > -1) {
+array.splice(index, 1);
+}
+// array = [1, 5]
+console.log(array);
+
+
+
+background-image: url("../images/main_background.JPG");
+    background-attachment: fixed;
+    background-color: $backgroundColor;
+    background-position: top center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+
+
+
+    */
+    //generatedNotes.splice(noteToDelete, );
+    //console.log(noteToDelete);
+    //console.log(deletedNote);
+    //let noteToDelete = generatedNotes.splice(id, 1);
--- assets/script/notes.js
+++ assets/script/notes.js
@@ -1,6 +1,7 @@
 let notes = []; //массив для текста заметок
 let titles = []; //массив для заголовков
 let generatedNotes = []; //массив для созданного дива
+let noteString = ''; //переменная для генерируемой заметки
 
 document.addEventListener("DOMContentLoaded", function (event) {
     let addedNote = localStorage.getItem('note'); //переменная для ключа в хранилище
@@ -15,13 +16,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function addNote() { //функция добавления заметки
-    let note = document.getElementById("add-box__note-input").value; //переменная для введенного текста заметки
-    notes.push(note); //добавление в массив
     let title = document.getElementById("add-box__note-title").value; //переменная для введенного заголовка заметки
+    let note = document.getElementById("add-box__note-input").value; //переменная для введенного текста заметки
     titles.push(title); //добавление в массив
-    let noteString = ''; //переменная для генерируемой заметки
+    notes.push(note); //добавление в массив
+    
     for (i = 0; i < notes.length; i++) { //перебираем каждый элемент массива добавленных текстов, цикл с счетчиком, потому что нужно склеить массив заголовков и массив текстов
-        noteString = `<div class="note"><h3 id="note-title">${title}</h3><p id="note-text">${note}</p><button id="note-btn" type="button" onclick="deleteNote();"><img src="assets/images/note-icon-delete.svg" alt="delete"></button></div>`; //создаем заметку в виде дива с заголовком и кнопкой удалить (еще в процессе)
+        noteString = `<div id="note${i}" class="note"><h3 id="note-title">${title}</h3><p id="note-text">${note}</p><button id="note-btn" type="button" onclick="deleteNote(${i});"><img src="assets/images/note-icon-delete.svg" alt="delete"></button></div>`; //создаем заметку в виде дива с заголовком и кнопкой удалить (еще в процессе)
     }
     generatedNotes.push(noteString); //добавление в массив новых заметок
     document.getElementById("notes").innerHTML += noteString; //вывод в див
@@ -30,7 +31,16 @@ function addNote() { //функция добавления заметки
 }
 
 function clearAll() { //функция очищения всех заметок из хранилища и из документа
-    localStorage.removeItem('note'); //очищаем всё хранилище
+    localStorage.removeItem('note'); //очищаем ключ
     document.querySelectorAll(".note").forEach(note => { note.style.display = "none" }); //выбираем все созданные дивы, перебираем и присваиваем каждому стиль display: none;
     //нагуглила просто замечательный способ
+}
+
+function deleteNote(id) {
+    generatedNotes = JSON.parse(localStorage.getItem('note'));
+    for (i = 0; i < generatedNotes.length; i++) {
+        generatedNotes.splice(id, 1);
+        document.querySelector("#note" + id).style.display = "none";
+        localStorage.setItem('note', JSON.stringify(generatedNotes));
+    }
 }
